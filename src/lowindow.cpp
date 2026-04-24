1ec1472986c5c2ee112751339e1182eee5a905eb
lorolol <matheus.pauli6@gmail.com>
2026-04-24 14:49:40 -0300

    Fix LoWindow to get signals correctly

--- src/lowindow.cpp
+++ src/lowindow.cpp
@@ -1,7 +1,7 @@
 #include "lowindow.h"
 
 void LoWindow::RlInitWindow() {
-  unsigned int flags = FLAG_VSYNC_HINT | FLAG_VSYNC_HINT | FLAG_MSAA_4X_HINT;
+  unsigned int flags = FLAG_VSYNC_HINT | FLAG_MSAA_4X_HINT | FLAG_WINDOW_RESIZABLE;
   SetConfigFlags(flags);
   this->main = new std::thread([this](){this->WindowLoop();});
 }
@@ -11,14 +11,20 @@ void LoWindow::WindowLoop() {
   for (;;) {
     Vector2 m_pos = GetMousePosition();
     float scroll = GetMouseWheelMove();
-    bool m_btn[3] {IsMouseButtonPressed(MOUSE_BUTTON_LEFT), 
-      IsMouseButtonPressed(MOUSE_BUTTON_RIGHT), 
-      IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE)};
+    bool m_btn[3] {IsMouseButtonDown(MOUSE_BUTTON_LEFT), 
+      IsMouseButtonDown(MOUSE_BUTTON_RIGHT), 
+      IsMouseButtonDown(MOUSE_BUTTON_MIDDLE)};
     
-    LoSignal sig = {};
+    LoSignal sig {};
+    sig.mouse_pos = m_pos;
     if (m_btn[static_cast<int>(MouseButtons::M1)]) {
-      
-    } 
+      sig.mouse_state = MouseState::M1_Pressed;
+    } else if (m_btn[static_cast<int>(MouseButtons::M2)]) {
+      sig.mouse_state = MouseState::M2_Pressed;
+    } else if (m_btn[static_cast<int>(MouseButtons::M3)]) {
+      sig.mouse_state = MouseState::M3_Pressed;
+    } else sig.mouse_state = MouseState::Hover;
+    
     this->Update(sig);
     BeginDrawing();
       ClearBackground(this->bg);
@@ -42,6 +48,9 @@ LoContainer(0, 0, w, h, (float[4]){0.0, 0.0, 0.0, 0.0}, name, objs) {
 
 
 void LoWindow::Update(LoSignal &sig) {
+  this->SetHeight(GetScreenHeight());
+  this->SetWidth(GetScreenWidth());
+  this->HandleSignal(sig);
   DrawRectangleLines(this->GetPosX() + this->GetPadding(Pad::Left),
                     this->GetPosY() + this->GetPadding(Pad::Top),
                     this->GetWidth() - (this->GetPadding(Pad::Left) + this->GetPadding(Pad::Right)),
